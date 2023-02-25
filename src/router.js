5c95142ac8007fa5435385ae5421faad03b6aad2
Athena Serene <aserenehoosang@gmail.com>
2023-02-25 12:01:25 -0800

    added index & show loader to router.js

--- src/router.js
+++ src/router.js
@@ -1,13 +1,14 @@
 import {createBrowserRouter, createRoutesFromElements, Route} from "react-router-dom"
 import App from "./App"
+import { IndexLoader, ShowLoader } from "./loaders"
 import Index from "./pages/Index"
 import Show from "./pages/Show"
 
 const router = createBrowserRouter(createRoutesFromElements(
     <>
         <Route path="/" element={<App/>}>
-            <Route path="" element={<Index/>}/>
-            <Route path="post/:id" element={<Show/>}/>
+            <Route path="" element={<Index/>} loader={IndexLoader}/>
+            <Route path="post/:id" element={<Show/>} loader={ShowLoader}/>
             <Route path="create"/>
             <Route path="update/:id"/>
             <Route path="delete/:id"/>
