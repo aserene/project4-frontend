d274405ed8e6b631d60171469da1ae75d94aacd9
Athena Serene <aserenehoosang@gmail.com>
2023-02-26 18:13:16 -0800

    added dropdown feature to form

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -10,15 +10,28 @@ const Index = (props) => {
         <div className="add-wares">
             <h2>Add Wares</h2>
             <Form action="/create" method="post">
-                <label>Name of Item</label>
-                <input type="text" name="name" placeholder="Name"/>
-                <label for="description">Description:</label>
-                <textarea id="description" name="description" rows="5" cols="50"></textarea>
-                <label>Category</label>
-                <input type="text" name="category" placeholder="Category"/>
+                <label>Name of Item</label><br/>
+                <input type="text" name="name" placeholder="Name"/><br/>
+                <label for="description">Description:</label><br/>
+                <textarea id="description" name="description" rows="5" cols="50"></textarea><br/>
+                <label for="category">Choose a Category:</label><br/>
+                <select id="category" name="category">
+                <option value="clothing">Clothing</option>
+                <option value="shoes">Shoes</option>
+                <option value="jewlry">Jewelry</option>
+                <option value="toys">Toys</option>
+                <option value="kitchen">Kitchen</option>
+                <option value="games & electronics">Games & Electronics</option>
+                <option value="hardware">Hardware</option>
+                <option value="accessories">Accessories</option>
+                <option value="pet supplies">Pet Supplies</option>
+                <option value="food & grocery">Food & Grocery</option>
+                <option value="beauty & health">Beauty & Health</option>
+                <option value="automotive & industrial">Automotive & Industrial</option>
+                </select>
                 <label>Price</label>
-                <input type="text" name="price"/>
-                <label>Image Url</label>
+                <input type="text" name="price"/><br/>
+                <label>Image Url</label><br/>
                 <input type="text" name="img" placeholder="https://"/>
                 <button>Add Ware</button>
             </Form>
